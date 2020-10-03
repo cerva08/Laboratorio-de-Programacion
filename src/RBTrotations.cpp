eb2780caa2dd1074e17ead752b603a235e2eae7e
Marisol Zeledón <mari.sol_17@hotmail.com>
2020-10-03 00:00:33 -0600

    Correccion, se agrega un if

--- src/RBTrotations.cpp
+++ src/RBTrotations.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <iostream>
-#include "include/RBT.h"
+#include "../include/RBT.h"
 
 using namespace std;
 
@@ -11,8 +11,11 @@ int RBT_right_rotate(
 
     struct RBT_node* temp = rotation_node->lc_node;
     rotation_node->lc_node = temp->rc_node;
-    temp->rc_node->p_node = rotation_node;
     temp->p_node = rotation_node->p_node;
+
+    if (temp->rc_node != NULL){
+        temp->rc_node->p_node = rotation_node;
+    }
 
     if (rotation_node->p_node == NULL){
         in_root = temp;
@@ -36,8 +39,11 @@ int RBT_left_rotate(
 
     struct RBT_node* temp = rotation_node->rc_node;
     rotation_node->rc_node = temp->lc_node;
-    temp->lc_node->p_node = rotation_node;
     temp->p_node = rotation_node->p_node;
+
+    if (temp->lc_node != NULL){
+        temp->lc_node->p_node = rotation_node;
+    }
 
     if (rotation_node->p_node == NULL){
         in_root = temp;
