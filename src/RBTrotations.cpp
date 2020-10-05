545e5755b621d71830390a1099888ab941d9d3bb
Marisol Zeledón <mari.sol_17@hotmail.com>
2020-10-04 18:28:26 -0600

    cambios rotations, se puede rotar en root

--- src/RBTrotations.cpp
+++ src/RBTrotations.cpp
@@ -7,7 +7,13 @@ using namespace std;
 
 int RBT_right_rotate(
     struct RBT_node*  in_root, 
-    struct RBT_node*  rotation_node){
+    struct RBT_node*  rotation_node,
+    struct RBT_node* new_root){
+
+    bool is_root = false;
+    if (in_root == rotation_node){
+        is_root = true;
+    }
 
     struct RBT_node* temp = rotation_node->lc_node;
     rotation_node->lc_node = temp->rc_node;
@@ -28,6 +34,10 @@ int RBT_right_rotate(
     temp->rc_node = rotation_node;
     rotation_node->p_node = temp;
 
+    if (is_root){
+        new_root_setter(temp,new_root);
+    }
+
     return 0;
 
 }
@@ -35,7 +45,13 @@ int RBT_right_rotate(
 
 int RBT_left_rotate(
     struct RBT_node*  in_root, 
-    struct RBT_node*  rotation_node){
+    struct RBT_node*  rotation_node,
+    struct RBT_node* new_root){
+
+    bool is_root = false;
+    if (in_root == rotation_node){
+        is_root = true;
+    }
 
     struct RBT_node* temp = rotation_node->rc_node;
     rotation_node->rc_node = temp->lc_node;
@@ -55,6 +71,24 @@ int RBT_left_rotate(
 
     temp->lc_node = rotation_node;
     rotation_node->p_node = temp;
+
+    if (is_root){
+        new_root_setter(temp,new_root);
+    }
+
+    return 0;
+
+}
+
+int new_root_setter (
+    struct RBT_node* new_root_setter_values,
+    struct RBT_node* new_root){
+
+    new_root->color = new_root_setter_values->color;
+    new_root->value = new_root_setter_values->value; 
+    new_root->lc_node = new_root_setter_values->lc_node;
+    new_root->rc_node = new_root_setter_values->rc_node; 
+    new_root->p_node = new_root_setter_values->p_node;
 
     return 0;
 
