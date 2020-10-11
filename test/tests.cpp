dd31c159e388feeb9be7cbea1131df8f9f24b1bf
Carolina Paniagua <paniaguacarolina10@gmail.com>
2020-10-11 16:25:26 -0600

    pruebas delete

--- test/tests.cpp
+++ test/tests.cpp
@@ -134,6 +134,77 @@ TEST(RBTsearch, negative){
     EXPECT_EQ(status2, -1);
 }
 
+TEST(RBTremove, positive){
+    struct RBT_node N13;
+    struct RBT_node N8;
+    struct RBT_node N17;
+    struct RBT_node N1;
+    struct RBT_node N11;
+    struct RBT_node N15;
+    struct RBT_node N25; 
+    struct RBT_node N6; 
+    struct RBT_node N22;
+    struct RBT_node N27;
+    struct RBT_node min_node, max_node, found_node; 
+    int  status;
+    
+
+   
+    N13 = {&N8, &N17, NULL, 13, 'B'}; // root  
+    N8 = {&N1, &N11, &N13, 8, 'R'};       
+    N17 = {&N15, &N25, &N13, 17, 'R'}; 
+    N1 = {NULL, &N6, &N8, 1, 'B'}; 
+    N11 = {NULL, NULL, &N8, 11, 'B'}; 
+    N15 = {NULL, NULL, &N17, 15, 'B'}; 
+    N25 = {&N22, &N27, &N17, 25, 'B'}; 
+    N6 = {NULL, NULL, &N1, 6, 'R'}; 
+    N22 = {NULL, NULL, &N25, 22, 'R'};
+    N27 = {NULL, NULL, &N25, 27, 'R'};
+    found_node = {NULL, NULL, NULL, 0, 'R'};
+    min_node = {NULL, NULL, NULL, 0, 'R'};
+    max_node = {NULL, NULL, NULL, 0, 'R'};
+    status = RBT_node_remove(&N13,15,&N13);
+
+    EXPECT_EQ(status, 0);
+
+}
+
+TEST(RBTremove, negative){
+    struct RBT_node N13;
+    struct RBT_node N8;
+    struct RBT_node N17;
+    struct RBT_node N1;
+    struct RBT_node N11;
+    struct RBT_node N15;
+    struct RBT_node N25; 
+    struct RBT_node N6; 
+    struct RBT_node N22;
+    struct RBT_node N27;
+    struct RBT_node min_node, max_node, found_node; 
+    int  status1, status2;
+    
+
+   
+    N13 = {&N8, &N17, NULL, 13, 'B'}; // root  
+    N8 = {&N1, &N11, &N13, 8, 'R'};       
+    N17 = {&N15, &N25, &N13, 17, 'R'}; 
+    N1 = {NULL, &N6, &N8, 1, 'B'}; 
+    N11 = {NULL, NULL, &N8, 11, 'B'}; 
+    N15 = {NULL, NULL, &N17, 15, 'B'}; 
+    N25 = {&N22, &N27, &N17, 25, 'B'}; 
+    N6 = {NULL, NULL, &N1, 6, 'R'}; 
+    N22 = {NULL, NULL, &N25, 22, 'R'};
+    N27 = {NULL, NULL, &N25, 27, 'R'};
+    found_node = {NULL, NULL, NULL, 0, 'R'};
+    min_node = {NULL, NULL, NULL, 0, 'R'};
+    max_node = {NULL, NULL, NULL, 0, 'R'};
+    status1 = RBT_node_remove(&N13,30,&N13);
+    EXPECT_EQ(status1, -4);
+    status2 = RBT_node_remove(&N13,-3,&N13);
+    EXPECT_EQ(status2, -1);
+
+}
+
 
 
 int main(int argc, char **argv) {
